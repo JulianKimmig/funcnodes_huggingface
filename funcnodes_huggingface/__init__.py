b6e338c7b00cbd5fe6090eac3155b2fa813f6704
JulianKimmig <julian.kimmig@gmx.net>
2024-08-29 12:47:23 +0200

    init

---
+++ funcnodes_huggingface/__init__.py
@@ -0,0 +1,8 @@
+import funcnodes as fn
+
+NODE_SHELF = fn.Shelf(
+    nodes=[],
+    name="Funcnodes Huggingface",
+    description="The nodes of Funcnodes Huggingface package",
+    subshelves=[],
+)
